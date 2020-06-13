eaa6f6075acbbb236dc0d7438d06fdf342a51e3f
bmapa-iscteiul <bmapa@iscte-iul.pt>
2020-06-13 10:56:21 +0100

    HTML Table complete , Javadoc, JUnit tests and cgi_lib added

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.covidgraphspreadv2","l":"app"}]
--- src/com/covidgraphspreadv2/app.java
+++ src/com/covidgraphspreadv2/app.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -29,6 +30,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import com.covidgraphspreadv2.cgi_lib;
 public class app {
 
 	public static Git git;
@@ -39,9 +41,19 @@ public class app {
 		setHTMLTablesCSS();
 		cloneRepository();
 		getAllFilesFromTags();
-		createHTMLFile();
+		//createHTMLFile();
+		System.out.println(cgi_lib.Header());
+		Hashtable form_data = cgi_lib.ReadParse(System.in);
+		
+		System.out.println(doc.select("table"));
+		System.out.println(doc.select("style"));
+		
+		System.out.println(cgi_lib.HtmlBot());
 	}
 	
+	/**
+	 * Lists all the tags and searches for the file associated 
+	 */
 	public static void getAllFilesFromTags() {
 		Repository repository = git.getRepository();
 		try {
@@ -49,20 +61,19 @@ public class app {
 			List<Ref> call = git.tagList().call();
 			
 			for (Ref ref : call) {
-				System.out.println("Tag: " + ref.getName());
+				//System.out.println("Tag: " + ref.getName());
 				RevWalk walk = new RevWalk(repository);
 				try {
 					RevObject object = walk.parseAny(ref.getObjectId());
 					
 					//gets the commit associated to the tag
 					if (object instanceof RevCommit) {
-						System.out.println("RevCommit: " + ref.getName());
+						//System.out.println("RevCommit: " + ref.getName());
 						//retrieves file from commit using the commit ObjectId
 						getFileFromCommit(ref.getObjectId(), ref.getName());
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				} catch (Exception E) {
+					
 				}
 			}
 				
@@ -73,6 +84,13 @@ public class app {
 		
 	}
 	
+	/**
+	 * given a commit id and its tag, searches for covid19spreading.rdf file
+	 * and if found, adds the commit to the table
+	 * 
+	 * @param commitId - the id of the commit
+	 * @param tag - the tag associated to the commit
+	 */
 	public static void getFileFromCommit(ObjectId commitId, String tag) {
 		Repository repository = git.getRepository();
 	
@@ -80,7 +98,8 @@ public class app {
 			RevCommit commit = revWalk.parseCommit(commitId);
 			
 			RevTree tree = commit.getTree();
-			System.out.println("Tree: " + tree);
+			System.out.println(commit.getId());
+			//System.out.println("Tree: " + tree);
 			
 			try(TreeWalk treeWalk = new TreeWalk(repository)){
 				treeWalk.addTree(tree);
@@ -91,7 +110,7 @@ public class app {
 				}
 				
 				ObjectId objectId = treeWalk.getObjectId(0);
-				System.out.println(getHyperlinkOfFileFromCommit(commit));
+				//System.out.println(getHyperlinkOfFileFromCommit(commit));
 				ObjectLoader loader = repository.open(objectId);
 				addCommitToTable(commit, tag);
 				//loader.copyTo(System.out);
@@ -102,6 +121,9 @@ public class app {
 		}
 	}
 	
+	/**
+	 * Clones the remote repository localy to c:/path/to/repo
+	 */
 	public static void cloneRepository() {
 		File f = new File("/path/to/repo");
 		if (f.exists() && f.isDirectory()) {
@@ -123,6 +145,9 @@ public class app {
 		}
 	}
 	
+	/**
+	 * Creates the table 
+	 */
 	public static void createHTMLTable() {
 		doc = Jsoup.parse("<html></html>");
 		doc.body().addClass("body-styles-cls");
@@ -138,8 +163,13 @@ public class app {
 		headersRow.append("<th>Tag Description</th>");
 		headersRow.append("<th>Spread Visualization Link</th>");
 		//System.out.println(doc.toString());
+		
+		
 	}
 	
+	/**
+	 * Creates the covid-graph-spread.html file and writes all the Document(Jsoup) info
+	 */
 	public static void createHTMLFile() {
 		File out = new File("covid-graph-spread.html");
 	
@@ -156,6 +186,12 @@ public class app {
 		    
 	}
 	
+	/**
+	 * @param tableIndex - the index of the table we want to add the row
+	 * @param tableData - data array of the row
+	 * 
+	 * Given the table index, and the row data, adds a row to the table
+	 */
 	public static void addRowToHTMLTable(int tableIndex, String[] tableData) {
 		Element table = doc.select("table").get(tableIndex);
 		Element newRow = table.append("<tr></tr>");
@@ -163,12 +199,15 @@ public class app {
 		for(int i =0; i < tableData.length; i++) {
 			table.select("tr").get(newRowIndex).append("<td>" + tableData[i] + "</td>");
 		}
-		System.out.println(doc.toString());
+		//System.out.println(doc.toString());
 	}
 	
+	/**
+	 * Css stylying of the table
+	 */
 	public static void setHTMLTablesCSS() {
 		Element style = doc.select("style").get(0);
-		style.append("table, th, td {border: 1px solid black; border-collapse: collapse;}");
+		style.append("table, th, td {border: 1px solid #add8e6; border-collapse: collapse;}");
 		style.append("th, td {\r\n" + 
 				"  padding: 15px;\r\n" + 
 				"}");
@@ -189,8 +228,12 @@ public class app {
 				"  background-color: #add8e6;\r\n" + 
 				"}");
 		
+		
 	}
 	
+	/**@param commit - the commit we want to get the timestamp
+	 * @return Returns the timestamp of a given commit
+	 */
 	public static String getCommitTimestamp(RevCommit commit) {
 		PersonIdent authorIdent = commit.getAuthorIdent();
 		Date authorDate = authorIdent.getWhen();
@@ -198,17 +241,22 @@ public class app {
 		return authorDate.toString();
 	}
 	
-	public static String getCommitDescription(RevCommit commit) {
-		System.out.println("Descricao: " + commit.getFullMessage());
-		return commit.getFullMessage();
-	}
-	
+	/** @param commit - the commit
+	 *  @return hyperlink - the concatenation of WebVOWL link + raw github file link
+	 * Creates the spreading visualization link on WebVOWL concatenating the raw image hyperlink 
+	 */
 	public static String getHyperlinkOfFileFromCommit(RevCommit commit) {
 		String commitId = commit.getName();
 		String hyperlink = "http://www.visualdataweb.de/webvowl/#iri=https://raw.githubusercontent.com/vbasto-iscte/ESII1920/" + commitId + "/covid19spreading.rdf";
 		return hyperlink;
 	}
 	
+	/**@param commit - the commit
+	 * @param tag - the tag associated to the commit
+	 * 
+	 * Creates a data array that contains each cell of the row 
+	 * 1 - Tag timestamp 2 - Tag filename 3 - Tag message 4 - Tag description 5 - hyperlink to WebVOWL 
+	 */
 	public static void addCommitToTable(RevCommit commit, String tag) {
 		String[] rowData = new String[5];
 		rowData[0] = getCommitTimestamp(commit);
